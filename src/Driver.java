0f1a48b5ed2edf6b7eb1b9107e5e0aab79ca098d
qualgirg <ekacenamerrell@parktudor.org>
2020-09-25 09:51:30 -0400

    ai  v ai

--- src/Driver.java
+++ src/Driver.java
@@ -1,118 +1,24 @@
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
-
-        int row;
-        int col;
         Scanner in = new Scanner(System.in);
 
+        System.out.print("Human v AI(1) or AI v AI(2): ");
+        int playType = in.nextInt();
+        in.nextLine();
+        Game g1 = new Game();
 
-        Mark player = new Mark("R");
-        Mark botMark = new Mark("Y");
-
-        Board b = new Board(4,4);
-        b.getBoard();
-
-        Advanced proBot = new Advanced();
-
-        int count =0;
-        int playerCount =0;
-        int botCount = 0;
-
-        Random ran = new Random();
-
-        int ranNum = ran.nextInt(2);
+        if (playType == 2) {
 
 
-        while(count <=8){
-            if(ranNum == 0){
-                System.out.print("Please enter column: ");
-                col =  in.nextInt();
-                Move move = new Move(b.getRow(col),col);
-                b.markMove(move,player);
-                b.getBoard();
-
-
-                if(b.checkWin(player)){
-                    System.out.println("Player Wins!");
-                    break;
-                }
-
-                playerCount++;
-                if(playerCount+ botCount ==42){
-                    break;
-                }
-
-                System.out.println("Bot Turn");
-
-                System.out.println(proBot.minMax(b,botMark));
-                b.markMove(proBot.theMove(),botMark);
-
-
-                b.getBoard();
-
-
-                if(b.checkWin(botMark)){
-                    System.out.println("Bot Wins!");
-                    break;
-                }
-                botCount++;
-
-                if(playerCount+ botCount ==16){
-                    break;
-                }
-
-            }
-
-
-            //bot goes first
-            else{
-
-
-                System.out.println("Bot Turn");
-                ArrayList<Boolean> winBot = new ArrayList<>();
-                proBot.minMax(b,botMark);
-                System.out.println(proBot.theMove().getCol()+"yeeeet");
-                b.markMove(proBot.theMove(),botMark);
-
-                b.getBoard();
-
-
-
-                if(b.checkWin(botMark)){
-                    System.out.println("Bot Wins!");
-                    break;
-                }
-                botCount++;
-
-                if(playerCount+ botCount ==16){
-                    break;
-                }
-
-                ArrayList<Boolean> winPlayer = new ArrayList<>();
-                System.out.print("Please enter column: ");
-                col =  in.nextInt();
-                Move move = new Move(b.getRow(col),col);
-                b.markMove(move,player);
-                b.getBoard();
-
-                if(b.checkWin(player)){
-                    System.out.println("Player Wins!");
-                    break;
-                }
-                playerCount++;
-                if(playerCount+ botCount ==16){
-                    break;
-                }
-            }
-            count++;
+            g1.playAI();
         }
-        if(playerCount + botCount ==16){
+        else {
 
-            System.out.println("Draw!");
+            g1.playHuman();
+
+
         }
     }
 }
---
+++ src/Game.java
@@ -0,0 +1,181 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    public void playHuman(){
+        int col;
+        Scanner in = new Scanner(System.in);
+        Mark player = new Mark("R");
+        Mark botMark = new Mark("Y");
+
+        Board b = new Board(4, 4);
+        b.getBoard();
+
+        Advanced proBot = new Advanced();
+
+        int count = 0;
+        int playerCount = 0;
+        int botCount = 0;
+
+        Random ran = new Random();
+
+        int ranNum = ran.nextInt(2);
+
+
+        while (count <= 8) {
+            if (ranNum == 0) {
+                System.out.print("Please enter column: ");
+                col = in.nextInt();
+                Move move = new Move(b.getRow(col), col);
+                b.markMove(move, player);
+                b.getBoard();
+
+
+                if (b.checkWin(player)) {
+                    System.out.println("Player Wins!");
+                    break;
+                }
+
+                playerCount++;
+                if (playerCount + botCount == 16) {
+                    break;
+                }
+
+                System.out.println("Bot Turn");
+
+                System.out.println(proBot.minMax(b, botMark));
+                b.markMove(proBot.theMove(), botMark);
+
+
+                b.getBoard();
+
+
+                if (b.checkWin(botMark)) {
+                    System.out.println("Bot Wins!");
+                    break;
+                }
+                botCount++;
+
+                if (playerCount + botCount == 16) {
+                    break;
+                }
+
+            }
+
+
+            //bot goes first
+            else {
+
+
+                System.out.println("Bot Turn");
+
+                proBot.minMax(b, botMark);
+                b.markMove(proBot.theMove(), botMark);
+
+                b.getBoard();
+
+
+                if (b.checkWin(botMark)) {
+                    System.out.println("Bot Wins!");
+                    break;
+                }
+                botCount++;
+
+                if (playerCount + botCount == 16) {
+                    break;
+                }
+
+                System.out.print("Please enter column: ");
+                col = in.nextInt();
+                Move move = new Move(b.getRow(col), col);
+                b.markMove(move, player);
+                b.getBoard();
+
+                if (b.checkWin(player)) {
+                    System.out.println("Player Wins!");
+                    break;
+                }
+                playerCount++;
+                if (playerCount + botCount == 16) {
+                    break;
+                }
+            }
+            count++;
+        }
+        if (playerCount + botCount == 16) {
+
+            System.out.println("Draw!");
+        }
+    }
+
+
+
+
+
+    public void playAI(){
+
+        Mark bot2 = new Mark("R");
+        Mark botMark = new Mark("Y");
+
+        Board b = new Board(4,4);
+        b.getBoard();
+
+        Advanced proBot = new Advanced();
+        Advanced proBot2 = new Advanced();
+
+        int count =0;
+        int bot2Count =0;
+        int botCount = 0;
+
+
+
+
+        while(count <=8){
+            System.out.println("Bot Turn");
+
+            System.out.println(proBot.minMax(b,botMark));
+            System.out.println(proBot.theMove().toString());
+            b.markMove(proBot.theMove(),botMark);
+            b.getBoard();
+
+
+            if(b.checkWin(botMark)){
+                System.out.println("Bot1 Wins!");
+                break;
+            }
+
+            botCount++;
+            if(botCount+ bot2Count ==16){
+                break;
+            }
+
+            System.out.println("Bot2 Turn");
+
+            System.out.println(proBot2.minMax(b,bot2));
+            System.out.println(proBot2.theMove().toString());
+            b.markMove(proBot2.theMove(),bot2);
+
+
+            b.getBoard();
+
+
+            if(b.checkWin(bot2)){
+                System.out.println("Bot2 Wins!");
+                break;
+            }
+            bot2Count++;
+
+            if(bot2Count+ botCount ==16){
+                break;
+            }
+
+            count++;
+
+        }
+        if(bot2Count + botCount ==16){
+
+            System.out.println("Draw!");
+        }
+    }
+}
